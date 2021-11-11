4e3c447c75a06336cee47882be5f253ce207f96d
TJ-Yads <59428782+TJ-Yads@users.noreply.github.com>
2021-11-11 10:46:10 -0500

    Add files via upload

---
+++ BossAttacks.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAttacks : MonoBehaviour//used for the bosses attack patterns and is updated by the BossHealth script
+{
+    public GameObject TurretLaser, TurretSniper, FirePit, PulseWave, CryoPit;
+    public GameObject[] TurrentSpawns, FireSpawns, CryoSpawns;
+    GameObject currentPoint;
+    GameObject LastPoint;
+    public GameObject PulsePrep;
+    public Transform PulseSpawn;
+    public float FireWait;
+    public int FireCount;
+    public float TurretTimer;
+
+    int index;
+    // Start is called before the first frame update
+    void Start()//on start begin coroutines for turrets and firePits
+    {
+        StartCoroutine(SpawnTurrets());
+        StartCoroutine(SpawnFire());
+    }
+    //while active this will spawn turrets in once the timer ends and only if the spawn is missing a turret, turret timer drops over time as the boss weakens
+    public IEnumerator SpawnTurrets()
+    {
+        yield return new WaitForSeconds(3f);
+        while (true)
+        {
+            if(TurrentSpawns[0].transform.childCount < 1)
+            {
+                Instantiate(TurretLaser, TurrentSpawns[0].transform.position, TurrentSpawns[0].transform.rotation, TurrentSpawns[0].transform);
+            }
+            if (TurrentSpawns[1].transform.childCount < 1)
+            {
+                Instantiate(TurretLaser, TurrentSpawns[1].transform.position, TurrentSpawns[1].transform.rotation, TurrentSpawns[1].transform);
+            }
+            if (TurrentSpawns[2].transform.childCount < 1)
+            {
+                Instantiate(TurretLaser, TurrentSpawns[2].transform.position, TurrentSpawns[2].transform.rotation, TurrentSpawns[2].transform);
+            }
+            if (TurrentSpawns[3].transform.childCount < 1)
+            {
+                Instantiate(TurretLaser, TurrentSpawns[3].transform.position, TurrentSpawns[3].transform.rotation, TurrentSpawns[3].transform);
+            }
+            yield return new WaitForSeconds(TurretTimer);
+        }
+    }
+    //while active this will spawn turrets in once the timer ends and only if the spawn is missing a turret, this one activates when 2 gens are broken
+    public IEnumerator SpawnSnipers()//this has the same turret respawn timer as above but each turret is a sniper variant with lower fire rate and more damage
+    {
+        yield return new WaitForSeconds(3f);
+        while (true)
+        {
+            if (TurrentSpawns[4].transform.childCount < 1)
+            {
+                Instantiate(TurretSniper, TurrentSpawns[4].transform.position, TurrentSpawns[4].transform.rotation, TurrentSpawns[4].transform);
+            }
+            if (TurrentSpawns[5].transform.childCount < 1)
+            {
+                Instantiate(TurretSniper, TurrentSpawns[5].transform.position, TurrentSpawns[5].transform.rotation, TurrentSpawns[5].transform);
+            }
+            if (TurrentSpawns[6].transform.childCount < 1)
+            {
+                Instantiate(TurretSniper, TurrentSpawns[6].transform.position, TurrentSpawns[6].transform.rotation, TurrentSpawns[6].transform);
+            }
+            if (TurrentSpawns[7].transform.childCount < 1)
+            {
+                Instantiate(TurretSniper, TurrentSpawns[7].transform.position, TurrentSpawns[7].transform.rotation, TurrentSpawns[7].transform);
+            }
+            yield return new WaitForSeconds(TurretTimer);
+        }
+    }
+    //once a gen is broken cryo is spawned at each gen location
+    public void SpawnCryo()//these pits deal damage and slow the player
+    {
+        foreach (GameObject Spawns in CryoSpawns)
+        {
+            Instantiate(CryoPit, Spawns.transform.position, Spawns.transform.rotation);
+        }
+    }
+    //spawn fire will spawn fire pits at random location marked on the boss area
+    public IEnumerator SpawnFire()//this loop will spawn a set amount of pits based on fireCount and will wait for the delay to spawn more
+    {
+        yield return new WaitForSeconds(3f);
+        while (true)
+        {
+            for (int i = 0; i < FireCount; i++)
+            {
+                index = Random.Range(0, FireSpawns.Length);
+                currentPoint = FireSpawns[index];
+                if (currentPoint != LastPoint)
+                {
+                    Instantiate(FirePit, currentPoint.transform);
+                    LastPoint = currentPoint;
+                }
+                yield return new WaitForSeconds(1f);
+            }
+            yield return new WaitForSeconds(FireWait);
+        }
+    }
+    public void PulseStarter()
+    {
+        StartCoroutine(SpawnPulse());
+    }
+    public void SniperStarter()
+    {
+        StartCoroutine(SpawnSnipers());
+    }
+    public IEnumerator SpawnPulse()//start the FX for the pulse attack and then create the pulse which fires outwards from the center
+    {
+        PulsePrep.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        PulsePrep.SetActive(false);
+        Instantiate(PulseWave, PulseSpawn);
+    }
+}
---
+++ BossHealth.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealth : MonoBehaviour//the primary script for the boss enemy, this script pass data to other boss scripts to change attack patterns 
+{
+    public float GensAlive, PipesAlive, MainUnit;
+    public GameObject[] Pipes, Gens;
+    public GameObject Shield, BossShield, BreakFX;
+    public Image HealthBar, ShieldBar;
+    public bool BrokeTarget;
+    public BossAttacks BAttack;
+    public GameObject WinScreen;
+    public CMF.CameraController CameraController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ShieldPipes());
+    }
+    //when a gen is broken this will activate, places shields on them along with cryo pits, more fire pits and possibly the sniper turrets
+    public IEnumerator ShieldGenerators()
+    {
+        if(GensAlive == 2)
+        {
+            BAttack.SniperStarter();//starts sniper spawns in BossAttacks
+        }
+        BAttack.SpawnCryo();//in BossAttacks it creates cryo pits that slow and hurt the player
+        BAttack.FireCount += 2;//increase the max amount of fire pits the boss can spawn
+        BrokeTarget = true;
+        HealthShields();//updates the bosses health bar in game
+        yield return new WaitForSeconds(1f);
+        BrokeTarget = false;
+        foreach (GameObject Gen in Gens)//if a generator is still active then place a shield on it
+        {
+            Instantiate(Shield, Gen.transform);
+        }
+        if(GensAlive == 0)//if there is no generators then break the main shield
+        {
+            Instantiate(BreakFX, BossShield.transform.position, BossShield.transform.rotation);
+            Destroy(BossShield);
+        }
+    }
+    public IEnumerator ShieldPipes()
+    {
+        BAttack.TurretTimer -= 12.5f;//reduce the delay for more turrets to respawn
+        BAttack.FireCount += 4;//increase the max fire pit counter that can spawn
+        BrokeTarget = true;
+        HealthShields();
+        yield return new WaitForSeconds(1f);
+        BAttack.PulseStarter();//start the pulse wave, an attack the fires a large wave around the boss arena
+        BrokeTarget = false;
+        foreach (GameObject Pipe in Pipes)//place shields on each pipe that is still alive
+        {
+            Instantiate(Shield, Pipe.transform);
+        }
+    }
+    public void HealthShields()//update the bosses healthbars based on remaining pipes and generators
+    {
+        HealthBar.fillAmount = PipesAlive / 3;
+        ShieldBar.fillAmount = GensAlive / 4;
+    }
+    public void ShieldGens()
+    {
+        StartCoroutine(ShieldGenerators());
+    }
+    public void ShieldPipesM()
+    {
+        StartCoroutine(ShieldPipes());
+    }
+    public void WinMethod()
+    {
+        StartCoroutine(Win());
+    }
+    public IEnumerator Win()//upon winning slow the game for a moment then disable controls and present win menu
+    {
+        Time.timeScale = .33f;
+        yield return new WaitForSeconds(1.5f);
+        CameraController.enabled = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Time.timeScale = 0;
+        WinScreen.SetActive(true);
+    }
+}
